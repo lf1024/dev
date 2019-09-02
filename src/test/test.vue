1577fe6de6f232287ffeb5f140aaaa9f7e6ad1c4
lufang <1455196280@qq.com>
2019-09-02 09:54:35 +0800

    first

---
+++ src/test/test.vue
@@ -0,0 +1,10 @@
+<template>
+    <div id="app-0">
+        <my-component class="baz boo" />
+    </div>
+</template>
+<script type="text/javascript">
+        Vue.component('my-component',{
+            template:`<p class="foo bar">Hi</p>`
+        })
+</script>
